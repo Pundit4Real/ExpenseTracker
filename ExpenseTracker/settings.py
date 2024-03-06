910e900570fe97bf80380c16d4798821b83fd991
Mohammed Ali <mohammedaalli088@gmail.com>
2024-03-06 14:54:44 +0000

    feat: verification email configured

--- ExpenseTracker/settings.py
+++ ExpenseTracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,7 +90,11 @@ DATABASES = {
 
 
 REST_FRAMEWORK={
-    'NON_FIELD_ERRORS_KEY':'error'
+    'NON_FIELD_ERRORS_KEY':'error',
+
+       'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        )
 }
 
 # Password validation
@@ -132,3 +137,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_USE_TLS=True
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_HOST_USER=os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSSWORD=os.environ.get('EMAIL_HOST_PASSWORD')
